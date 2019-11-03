9153433477976f6d00b68dc6438d6a29eba88e90
Cybertox <oxetan@gmail.com>
2019-11-03 19:11:34 +0200

    Add files via upload
    Optional code for web-hooks

---
+++ bot-webhooks.py
@@ -0,0 +1,42 @@
+import Decypher
+import telebot
+import datetime
+import pytz
+import re
+#import config
+import os
+
+from config import TOKEN, TIMEZONE, TIMEZONE_COMMON_NAME
+from telegram.ext import Updater
+
+
+
+P_TIMEZONE = pytz.timezone(TIMEZONE)
+TIMEZONE_COMMON_NAME = TIMEZONE_COMMON_NAME
+
+bot = telebot.TeleBot(TOKEN)
+PORT = int(os.environ.get('PORT', '8443'))
+updater = Updater(TOKEN, use_context=True)
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(message.chat.id, "To decypher password send me  command /decypher XXX")
+
+@bot.message_handler(content_types=['text'])
+def decypher_code(message):
+    txt = message.text.split(' ', 1)
+    msg = Decypher.decypher(txt[1])
+   # if re.match(r'/decypher', message):
+     #   txt = re.split(r'/decypher', message)
+    bot.send_message(message.chat.id, msg)
+
+
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+
+updater.bot.set_webhook("https://decypher-boherokuapp.com/" + TOKEN)
+updater.idle()
+
+#bot.polling(none_stop=True)
+
