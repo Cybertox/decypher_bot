86c0c2de77b51b03274017efe0530fec03c66d3a
Cybertox <oxetan@gmail.com>
2019-11-08 01:02:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-TOKEN = '754265584:AAHnnOT40LIcGsFNCM9UpTdjxPoXD4RWSoY'
-TIMEZONE = 'Europe/Kiev'
-TIMEZONE_COMMON_NAME = 'Kiev'
+import os
+
+# webhook
+URL = os.environ.get('URL')
+PORT = int(os.environ.get('PORT', '5000'))
+
+# telegram
+TOKEN = os.environ.get('TOKEN')
