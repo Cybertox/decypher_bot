e01d72b1c038d1c4a63635f32379923c95c7c1e6
Cybertox <oxetan@gmail.com>
2019-11-10 22:29:56 +0200

    Add files via upload

--- Decypher.py
+++ Decypher.py
@@ -1,12 +1,39 @@
 import roman
 
-#text = "CXVIII CXVII CVII LII LII CV CII CXV L XLVIII XLIX LVII LVI LII LIV CXVIII CVI"
-def  decypher(text_data):
+#text1 = "CXVIII CXVII CVII LII LII CV CII CXV L XLVIII XLIX LVII LVI LII LIV CXVIII CVI"
+#text2 = "-.../..../..-./..././...-/./-./-./../-././../..-./.../-/.--/---/--.././.-./---/---/-././-./../-././-/..../.-././././../--./..../-/-/..../.-./././-../-"
+def  roman2arabic(text_data):
     txt_splited = text_data.split()
     print(txt_splited)
     decyphered_text = ""
     for rd in txt_splited:
         plain_ch = chr(roman.fromRoman(rd))
         decyphered_text += plain_ch
+    print(decyphered_text)
+    return decyphered_text
+def morze2arabic(text_data):
+    MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                       'C': '-.-.', 'D': '-..', 'E': '.',
+                       'F': '..-.', 'G': '--.', 'H': '....',
+                       'I': '..', 'J': '.---', 'K': '-.-',
+                       'L': '.-..', 'M': '--', 'N': '-.',
+                       'O': '---', 'P': '.--.', 'Q': '--.-',
+                       'R': '.-.', 'S': '...', 'T': '-',
+                       'U': '..-', 'V': '...-', 'W': '.--',
+                       'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                       '1': '.----', '2': '..---', '3': '...--',
+                       '4': '....-', '5': '.....', '6': '-....',
+                       '7': '--...', '8': '---..', '9': '----.',
+                       '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                       '?': '..--..', '/': '-..-.', '-': '-....-',
+                       '(': '-.--.', ')': '-.--.-'}
+    separator = '/'
+    txt_splited = text_data.split(separator)
+    print(txt_splited)
+    decyphered_text = ""
+    for rd in txt_splited:
+        for i in MORSE_CODE_DICT:
+            if rd == MORSE_CODE_DICT[i]:
+                decyphered_text += i
     print(decyphered_text)
     return decyphered_text
